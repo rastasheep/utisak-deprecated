5f2264a54605b468cd745281cb5817ca3d99e029
Aleksandar Diklic <rastasheep3@gmail.com>
2013-06-21 15:30:50 +0200

    Updated mail config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,8 +23,6 @@ Utisak::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-
   config.ember.variant = :development
 end
 
